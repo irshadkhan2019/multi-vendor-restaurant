43d44426baecdb7621e6a7a6dabee7c573a7cd17
Irshad Khan <khanirshad2019@gmail.com>
2023-05-01 17:16:57 +0530

    Adding custom User model

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
-    list_display=('email', 'first_name', 'last_name', 'username', 'role', 'is_active')
+    list_display = ("email", "first_name", "last_name", "username", "role")
     filter_horizontal = ()
     list_filter = ()
+    fieldsets = ()
 
-admin.site.register(User,CustomUserAdmin)
 
-    
+admin.site.register(User, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,31 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 # Create your models here.
 class UserManager(BaseUserManager):
-   def create_user(self, first_name, last_name, username, email, password=None):
-       if not email:
-           raise ValueError("User must have an email address")
-       if not username:
-            raise ValueError('User must have an username')
-       
-       user:User=self.model(
-           email=self.normalize_email(email),
-           username=username,
-           first_name = first_name,
-           last_name = last_name,
-       ) 
-       user.set_password(password)
-       user.save(using=self._db)
-       return user
-   
-   def create_superuser(self, first_name, last_name, username, email, password=None):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not username:
+            raise ValueError("User must have an username")
+
+        user: User = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, username, email, password=None):
         user = self.create_user(
-            email = self.normalize_email(email),
-            username = username,
-            password = password,
-            first_name = first_name,
-            last_name = last_name,
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
         )
         user.is_admin = True
         user.is_active = True
@@ -34,13 +35,14 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+
 class User(AbstractBaseUser):
-    VENDOR=1
-    CUSTOMER=2
+    VENDOR = 1
+    CUSTOMER = 2
 
     ROLE_CHOICE = (
-        (VENDOR, 'Vendor'),
-        (CUSTOMER, 'Customer'),
+        (VENDOR, "Vendor"),
+        (CUSTOMER, "Customer"),
     )
 
     first_name = models.CharField(max_length=50)
@@ -60,18 +62,16 @@ class User(AbstractBaseUser):
     is_active = models.BooleanField(default=False)
     is_superadmin = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
 
-    objects=UserManager()
+    objects = UserManager()
 
     def __str__(self):
         return self.email
-    
+
     def has_perm(self, perm, obj=None):
         return self.is_admin
 
     def has_module_perms(self, app_label):
         return True
-    
-    
