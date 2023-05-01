255a248e7e1ce55d502fefc289a2c26878dd96d5
Irshad Khan <khanirshad2019@gmail.com>
2023-05-01 18:12:02 +0530

    Adding UserProfile model for a User

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, UserProfile
 
 
 # Register your models here.
@@ -12,3 +12,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+# MEdia files
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
