2478cb21d0d0d178936ff1478bc812505a864407
Irshad Khan <khanirshad2019@gmail.com>
2023-05-02 23:53:29 +0530

    register User template ,form,view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from .models import User
 
 
 # Create your views here.
 def registerUser(request):
-    return HttpResponse("asdh")
+    if request.method == "POST":
+        print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            # assign customer role to user
+            user.role = User.CUSTOMER
+            form.save()
+            return redirect("accounts:registerUser")
+
+    else:
+        form = UserForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/registerUser.html", context)
