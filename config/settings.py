8104a039766ddd5cddd0a6f22aa7df20d1203733
Irshad Khan <khanirshad2019@gmail.com>
2023-05-03 22:14:01 +0530

    Using messages tags

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,13 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# message tags
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+    messages.SUCCESS: "success",
+    messages.INFO: "primary",
+    messages.WARNING: "warning",
+}
