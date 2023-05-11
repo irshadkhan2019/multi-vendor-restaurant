b4b91315d11b920f4d04dec4390cb6c76e3aa4cd
Irshad Khan <khanirshad2019@gmail.com>
2023-05-11 11:14:58 +0530

    edit delete categories

--- vendor/views.py
+++ vendor/views.py
@@ -91,3 +91,38 @@ def add_category(request):
         "form": form,
     }
     return render(request, "vendor/add_category.html", context)
+
+
+@login_required(login_url="login")
+@user_passes_test(check_role_vendor)
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == "POST":
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            category_name = form.cleaned_data["category_name"]
+            category = form.save(commit=False)
+            category.vendor = Vendor.objects.get(user=request.user)
+            category.slug = slugify(category_name)
+            form.save()
+            messages.success(request, "Category updated successfully!")
+            return redirect("accounts:vendor:menu_builder")
+        else:
+            print(form.errors)
+
+    else:
+        form = CategoryForm(instance=category)
+    context = {
+        "form": form,
+        "category": category,
+    }
+    return render(request, "vendor/edit_category.html", context)
+
+
+@login_required(login_url="login")
+@user_passes_test(check_role_vendor)
+def delete_category(request, pk=None):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    messages.success(request, "Category has been deleted successfully!")
+    return redirect("accounts:vendor:menu_builder")
