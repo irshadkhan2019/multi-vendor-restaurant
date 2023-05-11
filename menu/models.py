8c89b6e2ff05073da45551a65f25568092aaa076
Irshad Khan <khanirshad2019@gmail.com>
2023-05-11 10:05:35 +0530

    add category error handler

--- menu/models.py
+++ menu/models.py
@@ -15,6 +15,9 @@ class Category(models.Model):
         verbose_name = "category"
         verbose_name_plural = "categories"
 
+    def clean(self):
+        self.category_name = self.category_name.capitalize()
+
     def __str__(self):
         return self.category_name
 
