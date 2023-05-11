b3cced0c4abc5bc8e7e9d2bf2f34d6047d08938e
Irshad Khan <khanirshad2019@gmail.com>
2023-05-11 09:41:56 +0530

    fooditems bycategory url,view,template

--- vendor/views.py
+++ vendor/views.py
@@ -7,7 +7,7 @@ from .models import Vendor
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 from accounts.utils import check_role_vendor
-from menu.models import Category
+from menu.models import Category, FoodItem
 
 
 # Create your views here.
@@ -52,3 +52,16 @@ def menu_builder(request):
         "categories": categories,
     }
     return render(request, "vendor/menu_builder.html", context)
+
+
+@login_required(login_url="login")
+@user_passes_test(check_role_vendor)
+def fooditems_by_category(request, pk):
+    vendor = Vendor.objects.get(user=request.user)
+    category = get_object_or_404(Category, pk=pk)
+    fooditems = FoodItem.objects.filter(vendor=vendor, category=category)
+    context = {
+        "fooditems": fooditems,
+        "category": category,
+    }
+    return render(request, "vendor/fooditems_by_category.html", context)
