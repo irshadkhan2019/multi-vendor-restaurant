1854c64654fc58354c3ad2fe52a09c6d19212383
Irshad Khan <khanirshad2019@gmail.com>
2023-05-12 09:23:55 +0530

    Show Vendors on homepage

--- main/views.py
+++ main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from vendor.models import Vendor
+
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:5]
+    print(vendors)
+    context = {
+        "vendors": vendors,
+    }
+    return render(request, "home.html", context)
