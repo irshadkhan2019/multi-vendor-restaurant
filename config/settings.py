7e8eae755e43150d833732dfcca7e293cd8ba93c
Irshad Khan <khanirshad2019@gmail.com>
2023-05-17 08:27:12 +0530

    configure GDAL and postgis for geospatial queries

--- config/settings.py
+++ config/settings.py
@@ -164,3 +164,14 @@ EMAIL_USE_TLS = True
 # DEFAULT_FROM_EMAIL = "foodOnline Marketplace <dj.foodOnline.gmail.com>"
 
 GOOGLE_API_KEY = config("GOOGLE_API_KEY")
+
+# Configure gdal
+os.environ["PATH"] = (
+    os.path.join(BASE_DIR, "env\Lib\site-packages\osgeo") + ";" + os.environ["PATH"]
+)
+os.environ["PROJ_LIB"] = (
+    os.path.join(BASE_DIR, "env\Lib\site-packages\osgeo\data\proj")
+    + ";"
+    + os.environ["PATH"]
+)
+GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, "env\Lib\site-packages\osgeo\gdal304.dll")
