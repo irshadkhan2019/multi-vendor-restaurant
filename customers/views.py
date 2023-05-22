3e1c128d82500e247038225dd39ae7c40196b666
Irshad Khan <khanirshad2019@gmail.com>
2023-05-22 11:27:36 +0530

    save customer profile in db

--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserProfileForm, UserInfoForm
+from accounts.models import UserProfile
+from django.contrib import messages
 
 
 # Create your views here.
+@login_required(login_url="login")
 def cprofile(request):
-    return render(request, "customers/cprofile.html")
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == "POST":
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        user_form = UserInfoForm(request.POST, instance=request.user)
+
+        if profile_form.is_valid() and user_form.is_valid():
+            profile_form.save()
+            user_form.save()
+            messages.success(request, "Profile updated")
+            return redirect("accounts:customer:cprofile")
+        else:
+            print(profile_form.errors)
+            print(user_form.errors)
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        user_form = UserInfoForm(instance=request.user)
+
+    context = {
+        "profile_form": profile_form,
+        "user_form": user_form,
+        "profile": profile,
+    }
+    return render(request, "customers/cprofile.html", context)
